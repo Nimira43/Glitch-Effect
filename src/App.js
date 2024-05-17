dc9c39374140d7a3ab5e7a91a69349323f6239de
Nick Rabone <n.rabone72@aol.com>
2024-05-17 01:49:51 +0100

    Bringing in two styled components for h1 and button.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import styled from 'styled-components'
+
+const BasicTitle = styled.h1`
+  text-align: center;
+  text-transform: uppercase;
+  color: darkred;
+`
+
+const DefaultButton = styled.button`
+  background-color: darkred;
+  color: goldenrod;
+  padding: 10px 20px;
+  font-size: 1.5rem;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  display: block;
+  width: 200px;
+  margin: 1rem auto;
+`
+
 function App() {
   return (
-    <div style={{padding: '2rem', backgroundColor: 'darkred'}}>
-      <h1 className='title'>Styled Components</h1>
-      <button className='btn'>Click</button>
+    <div style={{padding: '2rem'}}>
+      <BasicTitle>Styled Components</BasicTitle>
+      <DefaultButton>Click</DefaultButton>
     </div>
   );
 }
+
+
 
 export default App;
