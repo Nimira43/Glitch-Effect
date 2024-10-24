77cda80faba74b62365d1f53b385bf68520282fd
Nimira43 <n.rabone72@aol.com>
2024-10-24 23:26:16 +0100

    Created BaseEntity.cs

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -11,4 +11,5 @@ public class Product
   public required string PictureUrl { get; set; }
   public required string Type { get; set; }
   public required string Brand { get; set; }
+  public int QuantityInStock { get; set; }
 }
--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3144102b33c02462836b7e356744356e204730a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd510d611ceafc13fdd5e2077df15c1c148563ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
