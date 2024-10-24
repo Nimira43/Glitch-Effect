cc7b3789d8fb2a7b5f39e39ec1c047919dd13cd7
Nimira43 <n.rabone72@aol.com>
2024-10-24 23:35:38 +0100

    Importing EntityFramework packaages for Design and SqlServer.

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3144102b33c02462836b7e356744356e204730a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d06a1881c68fd89a520c32978b2f43d267b4f4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
