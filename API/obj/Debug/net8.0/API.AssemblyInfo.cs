25c21ed5f43da42fc12f607f20a927fe3cf7bbf4
Nimira43 <n.rabone72@aol.com>
2024-10-24 23:12:10 +0100

    Removed Swagger item group from API

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe2e78ef639343b42569f85fb74710869328380")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c778e3c676cffb807eab80ead8c7136e49a67e44")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
