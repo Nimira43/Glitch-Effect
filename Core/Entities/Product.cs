b0ea1560fdf38f1173479dc9d63028555230ff10
Nimira43 <n.rabone72@aol.com>
2024-10-24 23:21:16 +0100

    Setting up fields for Product entity

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -4,5 +4,8 @@ namespace Core.Entities;
 
 public class Product
 {
-
+  public int Id { get; set; }
+  public required string Name { get; set; }
+  public required string Description { get; set; }
+  public decimal Price { get; set; }
 }
